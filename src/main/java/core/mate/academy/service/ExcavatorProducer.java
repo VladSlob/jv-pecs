72c999f4faf463a15a2262df722ced85ba2b8390
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-11-02 18:54:50 +0200

    ExcavatorProducer.java

---
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -0,0 +1,14 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Excavator;
+import java.util.Arrays;
+import java.util.List;
+
+public class ExcavatorProducer implements MachineProducer<Excavator> {
+
+    @Override
+    public List<Excavator> get() {
+        return Arrays.asList(new Excavator("Hitachi", "blue", 1.5),
+                new Excavator("JCB", "red", 1.3));
+    }
+}
