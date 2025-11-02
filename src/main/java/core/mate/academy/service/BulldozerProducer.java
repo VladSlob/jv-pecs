9be85c633ef3f69ccc6e6458fe4d4f199a898235
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-11-02 18:54:27 +0200

    BulldozerProducer.java

---
+++ src/main/java/core/mate/academy/service/BulldozerProducer.java
@@ -0,0 +1,14 @@
+package core.mate.academy.service;
+
+import core.mate.academy.model.Bulldozer;
+import java.util.Arrays;
+import java.util.List;
+
+public class BulldozerProducer implements MachineProducer<Bulldozer> {
+
+    @Override
+    public List<Bulldozer> get() {
+        return Arrays.asList(new Bulldozer("Cat", "Yellow", 3700),
+                new Bulldozer("Komatsu", "Gray", 3500));
+    }
+}
